a48ccae4f91d45dac26828390c2683b78664133f
FedeOrefici <federicoaorefici@gmail.com>
2023-07-17 13:56:09 -0300

    update

--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -4,7 +4,7 @@ const productSchema = require('../models/product')
 const router = express.Router()
 
 //create product
-router.post('/product', (req, res) => {
+router.post('/products', (req, res) => {
     const product = productSchema(req.body)
     product
         .save()
@@ -12,11 +12,38 @@ router.post('/product', (req, res) => {
         .catch((err) => res.json({message: err.message}))
 })
 
-router.get('/product', (req, res) => {
+router.get('/products/', (req, res) => {
     productSchema
     .find()
     .then((data) => res.json(data))
     .catch((err) => res.json({message: err.message}))
+})
+
+router.get('/products/:id', (req, res) => {
+    const { id } = req.params
+    productSchema
+    .findById(id)
+    .then((data) => res.json(data))
+    .catch((err) => res.json({message: err.message}))
+})
+
+
+router.put('/products/:id', (req, res) => {
+    const { id } = req.params
+    const { name, description, photo, price } = req.body
+    productSchema
+        .updateOne( {_id: id,}, { $set: {name, description, photo, price}})
+        .then((data) => res.json(data))
+        .catch((err) => ({message: err.message}))
+
+})
+
+router.delete('/products/:id', (req, res) => {
+    const { id } = req.params
+    productSchema
+        .deleteOne({_id: id})
+        .then((data) => res.json(data))
+        .catch((err) => ({message: err.message}))
 })
 
 module.exports = router
