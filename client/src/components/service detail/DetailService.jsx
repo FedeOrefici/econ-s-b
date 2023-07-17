8f7ddc1bbdff61e50c21ee002ddfc99ff0269c19
FedeOrefici <federicoaorefici@gmail.com>
2023-07-17 12:25:45 -0300

    update

--- client/src/components/service detail/DetailService.jsx
+++ client/src/components/service detail/DetailService.jsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from "react"
-import { ProductContext } from "../../context/contextProducts"
-import { useParams } from "react-router-dom"
+import { NavLink, useParams } from "react-router-dom"
 import axios from "axios"
+import Navbar from "../navbar/Navbar"
 
 const DetailService = () => {
 
@@ -19,16 +19,23 @@ const DetailService = () => {
   }, [id])
 
   return (
-    <div>
-      {
-        <div key={product._id}>
-          <p>{product.name}</p>
-          <p>{product.description}</p>
-          <p>{product.price}</p>
-        </div>  
-      }
+    <div className="">
+      <Navbar />
+        <div className="flex items-center justify-center w-full border border-red-600 h-full">
+        {
+          <div className="bg-slate-200 w-1/2 mx-auto shadow-lg rounded p-4" key={product?._id}>
+            <p>{product?.name}</p>
+            <p>{product?.description}</p>
+            <p>${product?.price}</p>
+            <div className="flex gap-1">
+              <button className="bg-slate-800 hover:bg-slate-700 text-white rounded py-2 w-[180px]">add to cart</button>
+              <NavLink to='/allServices' className="bg-slate-800 hover:bg-slate-700 text-white rounded py-2 w-[180px] text-center">more services</NavLink>
+            </div>
+          </div>  
+        }
+        </div>
     </div>
   )
 }
 
-export default DetailService
+export default DetailService 
--- client/src/components/user/UserProfile.jsx
+++ client/src/components/user/UserProfile.jsx
@@ -4,11 +4,10 @@ import { UsersContext } from "../../context/ContextUser"
 
 const UserProfile = () => {
 
-    
 
   return (
 
-      <div className="flex items-center justify-center gap-4 px-4 py-4 rounded bg-lime-400 w-[250px] cursor-pointer">
+      <div className="flex items-center justify-center gap-4 px-4 py-4 rounded bg-gray-50 w-[250px] h-[50px] cursor-pointer">
         <p className="text-gray-900">userProfile</p>
         <div className="flex text-gray-900">
             <span class="material-symbols-outlined">account_circle</span>
