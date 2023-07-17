a1e2a4075e17626b067c05a37fe82b76a12fd0c1
FedeOrefici <federicoaorefici@gmail.com>
2023-07-17 15:35:57 -0300

    update

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -6,10 +6,9 @@ const Cart = () => {
 
   const { handleCloseCart } = useContext(CartContext)
 
-  console.log(handleCloseCart, 'aca');
 
   return (
-    <div className="absolute bg-slate-50 h-[400px] w-[300px] top-7 right-[100px] p-4 rounded">
+    <div className="absolute bg-slate-50 h-[400px] w-[300px] top-7 right-[100px] p-4 rounded opacity-0 transition-opacity duration-300 hover:opacity-100">
         <button onClick={handleCloseCart} className="text-white bg-red-600 hover:bg-red-500 p-2 rounded">close</button>
     </div>
   )
