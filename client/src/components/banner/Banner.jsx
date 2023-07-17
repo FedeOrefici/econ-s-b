ff16937ac633ad4fdae5e0e31b50158241f78250
FedeOrefici <federicoaorefici@gmail.com>
2023-07-17 18:34:03 -0300

    update

--- client/src/components/banner/Banner.jsx
+++ client/src/components/banner/Banner.jsx
@@ -2,11 +2,12 @@ import back from '../../../assets/backhome.jpg'
 
 const Banner = () => {
   return (
-    <div className="text-white w-full h-screen bg-slate-900 flex flex-col items-center"
+    <div className="text-white w-full h-screen flex flex-col items-center justify-center"
     style={{backgroundImage: `url(${back})`, backgroundSize: 'cover', backgroundPosition: 'center'}}
     >
-      <div className="bg-[#E7E7E7] w-full p-10 h-[500px]">
-        <p className='text-slate-950'>carrousel sobre lo que se ofrece</p>
+      <div className="w-1/2 p-10 h-[500px] mt-20">
+        <p className='text-lime-400 text-6xl font-bold'>Asesorias online</p>
+        <p className='text-4xl italic'>pedí tu asesoria online sin moverte de tu casa.</p>
       </div>
       <div>
         <img />
