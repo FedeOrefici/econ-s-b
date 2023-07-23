e81076b2a305501a0195c78017af63243a1c63da
FedeOrefici <federicoaorefici@gmail.com>
2023-07-23 17:50:10 -0300

    update

--- client/src/components/user/UserProfile.jsx
+++ client/src/components/user/UserProfile.jsx
@@ -32,11 +32,14 @@ const UserProfile = () => {
         <p className="text-gray-900">{user?.name}</p>
         <div className="flex text-gray-900">
             <span class="material-symbols-outlined">account_circle</span>
-            {openOptions ? (<div className="bg-white text-center w-[250px] top-[75px] left-[674px] absolute">
-              <ul className="py-2 flex flex-col items-center justify-center">
+            {openOptions ? (<div className="bg-white text-center w-[250px] top-[75px] left-[1089px] absolute">
+              <ul className="py-2 flex flex-col items-center justify-center shadow-lg bg-slate-100">
                 <NavLink to='/createService' className="py-2 hover:bg-gray-300 w-full">Create Service</NavLink>
-                <NavLink to='/myServices' className="py-2 hover:bg-gray-300 w-full">Mi services</NavLink>
-                <NavLink onClick={logoutClean} className="py-2 hover:bg-gray-300 w-full">Logout</NavLink>
+                <NavLink to='/myServices' className="py-2 hover:bg-gray-300 w-full">My services</NavLink>
+                <NavLink onClick={logoutClean} className="py-2 hover:bg-gray-300 w-full flex items-center justify-center gap-4">
+                  <span class="material-symbols-outlined">logout</span>
+                  <p>logout</p>
+                </NavLink>
               </ul>
             </div>) : null}
         </div>
--- client/src/views/create service/CreateService.jsx
+++ client/src/views/create service/CreateService.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react"
 import Navbar from "../../components/navbar/Navbar"
 import validationsService from "./validations"
+import axios from "axios"
+import Swal from "sweetalert2"
+import { useNavigate } from "react-router-dom"
 
 const CreateService = () => {
+
+  const navigate = useNavigate()
 
   const [service, setService] = useState({
     name: '',
@@ -29,16 +34,43 @@ const CreateService = () => {
     }))
   }
 
-  console.log(service);
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const errors = validationsService(service)
+    if(Object.keys(errors).length > 0){
+      Swal.fire({
+        title: 'Empty fields',
+        icon: 'error',
+        confirm: 'accept'
+      })
+      return;
+    }
+    axios.post('http://localhost:3000/api/products/', service)
+    .then(res => {
+      Swal.fire({
+        title: 'Service Created',
+        icon: 'success',
+        confirm: 'accept'
+      })
+      navigate('/allServices')
+    })
+    setService({
+      name: '',
+      price: '',
+      description: '',
+      photo: ''
+    })
+    .catch(error => {
+      console.log({message: error.message});
+    })
+  }
 
-
-
-
+ 
   return (
     <>
     <Navbar />
     <div className="w-full h-screen">
-        <form className="w-2/3 h-screen rounded flex items-center justify-center flex-col mx-auto text-white">
+        <form onSubmit={handleSubmit} className="w-2/3 h-screen rounded flex items-center justify-center flex-col mx-auto text-white">
           <div className="flex flex-col gap-2 w-full h-[80px]">
             <input name="name" onChange={handleChange} value={service.name} type="text" className="py-2 text-black rounded px-2" placeholder="name service..." />
             {errors && <span className="text-yellow-500">{errors.name}</span>}
@@ -55,7 +87,7 @@ const CreateService = () => {
             <input name="photo" type="file" onChange={handleChange} value={service.photo} className="py-2 text-black rounded  px-2" placeholder="description..." />
             {errors && <span className="text-yellow-500">{errors.photo}</span>}
           </div>
-          <button className="bg-slate-300 text-black w-[180px] rounded py-2 hover:bg-slate-400 ">create</button>
+          <button type="submit" className="bg-slate-300 text-black w-[180px] rounded py-2 hover:bg-slate-400 ">create</button>
         </form>
     </div>
     </>
