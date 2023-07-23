f955cecd54ad0369fc89dc885b9adf41abf231f2
FedeOrefici <federicoaorefici@gmail.com>
2023-07-23 17:50:24 -0300

    update

--- api/src/models/product.js
+++ api/src/models/product.js
@@ -3,19 +3,19 @@ const mongoose = require('mongoose')
 const productSchema = mongoose.Schema({
     name: {
         type: String,
-        require: true,
+        required: true,
     },
     description: {
         type: String,
-        require: true,
+        required: true,
     },
     photo: {
         type: String,
-        require: true,
+        required: true,
     },
     price: {
         type: Number,
-        require: true,
+        required: true,
     }
 })
 
