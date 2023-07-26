11c83c47d2283ff22585435eccc9b2ac86a8a631
FedeOrefici <federicoaorefici@gmail.com>
2023-07-26 16:42:32 -0300

    update

--- client/src/views/AllServices/AllServices.jsx
+++ client/src/views/AllServices/AllServices.jsx
@@ -21,15 +21,14 @@ const AllServices = () => {
   return (
     <>
         <Navbar />
-        <div className="flex flex-col h-auto w-max-[50%] items-center justify-center gap-2 py-5 border border-red-600" style={{backgroundImage:`url(${background})`, backgroundSize: 'cover'}}>
+        <div className="flex flex-col h-auto w-max-[50%] items-center justify-center gap-2 py-5" style={{backgroundImage:`url(${background})`, backgroundSize: 'cover'}}>
               <div className="flex flex-row flex-wrap gap-2 items-center justify-center">
               {currenPageData?.map((product) => (
                 <Card product={product} />
               ))}
               </div>
-            
             <ReactPaginate
-            className="bg-red-600 flex items-center justify-center gap-10"
+            className="bg-slate-900 w-full m-10 py-10 rounded text-white flex items-center justify-center gap-20"
             previousLabel={'previous'}
             nextLabel={'next'}
             breakLabel={'...'}
@@ -38,7 +37,11 @@ const AllServices = () => {
             pageRangeDisplayed={3}
             onPageChange={handlePageChange}
             containerClassName={'pagination'}
-            activeClassName={'active'}
+            activeClassName={'bg-lime-500 p-2 rounded text-center hover:text-slate-900'}
+            pageLinkClassName={'pagination-item'}
+            previousLinkClassName={'hover:bg-lime-400 hover:text-slate-950 p-2 rounded'}
+            nextLinkClassName={'hover:bg-lime-400 hover:text-slate-950 p-2 rounded'}
+            breakLinkClassName={'hover:bg-red-500'}
             />
         </div>
     </>
