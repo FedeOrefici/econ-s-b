cedf4c610cf67f9cb6937f654680fafcc85ea75e
FedeOrefici <federicoaorefici@gmail.com>
2023-07-26 16:21:10 -0300

    update

--- client/src/components/user/UserProfile.jsx
+++ client/src/components/user/UserProfile.jsx
@@ -31,13 +31,13 @@ const UserProfile = () => {
       <div onClick={handleOpen} className="flex items-center justify-center gap-4 px-4 py-4 bg-slate-800 border rounded w-[250px] h-[50px] cursor-pointer">
         <p className="text-lime-400">{user?.name}</p>
         <div className="flex text-lime-400">
-            <span class="material-symbols-outlined">account_circle</span>
+            <span className="material-symbols-outlined">account_circle</span>
             {openOptions ? (<div className="bg-white text-center w-[243px] top-[75px] left-[1255px] absolute">
               <ul className="py-2 flex flex-col items-center justify-center bg-slate-800 border shadow-lg">
                 <NavLink to='/createService' className="py-2 hover:bg-white hover:text-slate-800 w-full">Create Service</NavLink>
                 <NavLink to='/myServices' className="py-2 hover:bg-white hover:text-slate-800 w-full">My services</NavLink>
                 <NavLink onClick={logoutClean} className="py-2 hover:bg-white hover:text-slate-800 w-full flex items-center justify-center gap-4">
-                  <span class="material-symbols-outlined">logout</span>
+                  <span className="material-symbols-outlined">logout</span>
                   <p>logout</p>
                 </NavLink>
               </ul>
--- client/src/views/AllServices/AllServices.jsx
+++ client/src/views/AllServices/AllServices.jsx
@@ -1,14 +1,41 @@
 import Card from "../../components/card/Card"
 import Navbar from "../../components/navbar/Navbar"
 import background from '../../../assets/backhome.jpg'
+import ReactPaginate from 'react-paginate'
+import { useContext, useState } from "react"
+import { ProductContext } from "../../context/ContextProducts"
+
 
 const AllServices = () => {
+
+  const { products } = useContext(ProductContext)
+  const itemsPerPage = 8
+  const [currentPage, setCurrentPage] = useState(0)
+  const offset = currentPage * itemsPerPage
+  const currenPageData = products?.slice(offset, offset + itemsPerPage)
+  
+  const handlePageChange = ({selected}) => {
+    setCurrentPage(selected)
+  }
 
   return (
     <>
         <Navbar />
         <div className="flex flex-row flex-wrap h-screen w-max-[50%] items-center justify-center gap-2 py-5" style={{backgroundImage:`url(${background})`, backgroundSize: 'cover'}}>
-            <Card />
+            {currenPageData?.map((product) => (
+              <Card product={product} />
+            ))}
+            <ReactPaginate
+            previousLabel={'previous'}
+            nextLabel={'next'}
+            breakLabel={'...'}
+            pageCount={Math.ceil(products?.length / itemsPerPage)}
+            marginPagesDisplayed={4}
+            pageRangeDisplayed={3}
+            onPageChange={handlePageChange}
+            containerClassName={'pagination'}
+            activeClassName={'active'}
+            />
         </div>
     </>
   )
