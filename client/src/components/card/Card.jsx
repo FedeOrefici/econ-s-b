510da501a9cf712b20b18ca4f043ba018a849449
FedeOrefici <federicoaorefici@gmail.com>
2023-07-26 21:41:25 -0300

    update

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -6,8 +6,9 @@ const Card = ({product}) => {
 
   return (
     <>
-      <div key={product?._id} className="bg-gray-200 w-[300px] h-[400px] flex items-center justify-center flex-col gap-10 rounded cursor-pointer hover:scale-105 hover:transition-transform" style={{backgroundImage: `url(${profile})`}}>
-        <div className="flex flex-col items-center justify-center w-full bg-white h-1/2">
+      <div key={product?._id} className="bg-gray-200 w-[300px] h-[400px] flex items-center justify-center flex-col gap-10 rounded cursor-pointer hover:scale-105 hover:transition-transform">
+        <div className="flex flex-col items-center justify-center w-full h-1/2">
+          <img src={profile} className="w-[30%] rounded" />
           <p className="font-bold text-2xl">{product?.name}</p>
           <NavLink to={`/products/${product._id}`}  className="font-medium">more</NavLink>
         </div>
