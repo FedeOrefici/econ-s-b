aea544d17ed2e887031b4d4fdec86391099c7209
FedeOrefici <federicoaorefici@gmail.com>
2023-07-28 17:58:45 -0300

    update

--- client/src/components/calendar/DatesCalendar.jsx
+++ client/src/components/calendar/DatesCalendar.jsx
@@ -1,9 +1,28 @@
+import { useState } from 'react'
 import Calendar from 'react-calendar'
 
 const DatesCalendar = () => {
+
+    const [value, setValue] = useState(new Date())
+    const [selectedDay, setSelectedDay] = useState(null)
+    
+    const handleChange = (date) => {
+        setValue(date)
+        setSelectedDay(value)
+    }
+
+    console.log(value);
+
   return (
-    <div>
-        <Calendar />
+    <div className='bg-slate-900 text-white w-full h-[500px] text-center flex items-center justify-center rounded shadow-lg'>
+        <Calendar
+        value={value}
+        onChange={handleChange}
+        onClickDay={handleChange}
+        />
+        <div>
+            <p>you selected the date: {value.toString()} </p>
+        </div>
     </div>
   )
 }
--- client/src/components/service detail/DetailService.jsx
+++ client/src/components/service detail/DetailService.jsx
@@ -35,8 +35,8 @@ const DetailService = () => {
         <div className="flex items-center justify-center w-full h-screen p-10 bg-slate-50">
         {
           <div className="flex flex-col items-center justify-center w-1/2 h-screen mx-auto rounded p-4" key={product?._id}>
-            <div className="flex flex-col items-center justify-center w-[900px] h-[670px] border border-red-600 p-10">
-              <img src={product?.photo} className="w-[300px] h-[300px] border border-red-500 shadow-lg" />
+            <div className="flex flex-col items-center justify-center w-[900px] h-[670px] p-10">
+              <img src={product?.photo} className="w-[300px] h-[300px] shadow-lg" />
               <div className="flex flex-col p-10 items-center justify-center text-center">
                 <p className="text-slate-900 text-4xl font-bold py-2">{product?.name}</p>
                 <p className="text-slate-900 text-center w-[400px]">{product?.description}</p>
@@ -47,7 +47,7 @@ const DetailService = () => {
             </div>
           </div>
         }
-        <div className="flex flex-col justify-center w-[900px] h-[670px] border border-red-600 p-10">
+        <div className="flex flex-col justify-center w-[900px] h-[670px] p-10">
           <div className="flex flex-col items-center justify-center left-10 gap-10 py-2 w-full">
             <DatesCalendar />
             <button onClick={() => addToCart(product)} className="bg-slate-900 hover:bg-slate-800 text-white rounded py-2 w-[180px]">add Service and date</button>
